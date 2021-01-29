4e3d195617dbe91274421d1769eedb881b9c3419
Munfa <munfa2014@gmail.com>
2021-01-29 21:17:07 +0600

    first commit

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- {{--bootstrap css CDN--}} -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" 
+    crossorigin="anonymous">
+  
+    <!-- {{--bootstrap js CDN--}} -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" 
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" 
+    crossorigin="anonymous"></script>
+
+    <title>ToDo List</title>
+</head>
+<body>
+    <div class="container">
+        <div class="col-md-8 offset-md-2">
+            <div class="row" style='margin-top: 1rem;'>
+                <h1>Todo List</h1>
+            </div>
+
+            <!-- display success message -->
+            @if (Session::has('success')) 
+                <div class="alert alert-success">
+                    <strong>Success:</strong> {{ Session::get('success') }}
+                </div>
+
+            @endif
+
+            <!-- display error message -->
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <strong>Error:</strong>
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+
+            @endif   
+
+            <div class="row">    
+                <form action="{{ route('tasks.update', [$taskUnderEdit->id]) }}" method="POST" class="col-md-12">
+                {{ csrf_field()}}
+                <input type="hidden" name="_method" value="PUT">
+                    <div class="form-group">
+                        <input type="text" name="updatedTaskName" class="form-control input-lg" value="{{ $taskUnderEdit->name }}">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="submit" value="Save Changes" class="btn btn-outline-success btn-lg">
+                        <a href="" class="btn btn-outline-danger btn-lg float-right">Go Back</a>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- {{--bootstrap css CDN--}} -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+    crossorigin="anonymous">
+
+    <style>
+        .pagination {
+            justify-content: center;
+        }
+        .pagination a {
+            display:inline;
+            padding: 0.5rem;
+        }
+    </style>
+
+    <title>ToDo List</title>
+</head>
+<body>
+    <div class="container">
+        <div class="col-md-8 offset-md-2">
+            <div class="row" style='margin-top: 1rem;'>
+                <h1>Todo List</h1>
+            </div>
+
+            <!-- display success message -->
+            @if (Session::has('success')) 
+                <div class="alert alert-success">
+                    <strong>Success:</strong> {{ Session::get('success') }}
+                </div>
+
+            @endif
+
+            <!-- display error message -->
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <strong>Error:</strong>
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+
+            @endif    
+
+            <form action="{{ route('tasks.store')}}" method="Post">
+            {{ csrf_field() }}
+                <div class="row" style='margin-top: 1rem; margin-bottom: 1rem;'>
+                    <div class="col-md-9">
+                        <input type="text" name="newTaskName" class="form-control">
+                    </div>
+
+                    <div class="col-md-3">
+                        <input type="submit" class="btn btn-primary btn-block" value="Add Task">
+                    </div>
+                </div>    
+            </form>
+            
+            <!-- display stored tasks -->
+            @if (count($storedTasks) > 0)
+                <table class="table">
+                    <tHead>
+                        <th>Task No</th>
+                        <th>Name</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tHead>
+
+                    <tBody>
+                        @foreach ($storedTasks as $storedTask)
+                            <tr>
+                                <th>{{ $storedTask->id }}</th>
+                                <td>{{ $storedTask->name }}</td>
+
+                                <!-- edit function -->
+                                <td><a href="{{ route('tasks.edit', ['tasks'=>$storedTask->id]) }}" class="btn btn-outline-secondary">Edit</a></td>
+
+                                <!-- delete function -->
+                                <td>
+                                    <form action="{{ route('tasks.destroy', ['tasks'=>$storedTask->id]) }}" method="POST">
+                                        {{ csrf_field() }}
+                                        <input type="hidden" name="_method" value="Delete">
+                                        <input type="submit" class="btn btn-danger" value="Delete">
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tBody>
+                </table>
+            @endif
+
+            <div class="row">
+                <!-- <div class="col-sm-1 text-center"> -->
+                    {{ $storedTasks->links() }}
+                <!-- </div> -->
+            </div>
+
+        </div>
+    </div>
+    
+    <!-- {{--bootstrap js CDN--}} -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" 
+    crossorigin="anonymous"></script>
+</body>
+</html>
